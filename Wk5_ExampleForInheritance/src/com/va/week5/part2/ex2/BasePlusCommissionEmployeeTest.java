3b540011b33a7a947f6296ced61c16b4888210e6
Vivek Ahuja <Vivek Ahuja@DESKTOP-VPMQ7T0>
2025-10-01 10:06:33 -0400

    update at 10:06

--- Wk5_ExampleForInheritance/src/com/va/week5/part2/ex2/BasePlusCommissionEmployeeTest.java
+++ Wk5_ExampleForInheritance/src/com/va/week5/part2/ex2/BasePlusCommissionEmployeeTest.java
@@ -1,17 +1,18 @@
-package com.va.week5.part2.ex1;
+package com.va.week5.part2.ex2;
 
-// Fig. 9.5: CommissionEmployeeTest.java
-// CommissionEmployee class test program.
+// Fig. 9.7: BasePlusCommissionEmployeeTest.java
+// BasePlusCommissionEmployee test program.
 
-public class CommissionEmployeeTest 
+public class BasePlusCommissionEmployeeTest 
 {
    public static void main(String[] args) 
    {
-      // instantiate CommissionEmployee object
-      CommissionEmployee employee = new CommissionEmployee(
-         "Sue", "Jones", "222-22-2222", 10000, .06);      
+      // instantiate BasePlusCommissionEmployee object
+      BasePlusCommissionEmployee employee = 
+         new BasePlusCommissionEmployee(
+         "Bob", "Lewis", "333-33-3333", 5000, .04, 300);
       
-      // get commission employee data
+      // get base-salaried commission employee data
       System.out.println(
          "Employee information obtained by get methods:");
       System.out.printf("%n%s %s%n", "First name is",
@@ -24,14 +25,16 @@ public class CommissionEmployeeTest
          employee.getGrossSales());
       System.out.printf("%s %.2f%n", "Commission rate is",
          employee.getCommissionRate());
+      System.out.printf("%s %.2f%n", "Base salary is",
+         employee.getBaseSalary());
 
-      employee.setGrossSales(5000); 
-      employee.setCommissionRate(0.12); 
+      employee.setBaseSalary(1000);
       
-      System.out.printf("%n%s:%n%n%s%n",                                
-         "Updated employee information obtained by toString", employee.toString());
+      System.out.printf("%n%s:%n%n%s%n", 
+         "Updated employee information obtained by toString", 
+         employee.toString());
    } // end main
-} // end class CommissionEmployeeTest
+} // end class BasePlusCommissionEmployeeTest
 
 
 /**************************************************************************
